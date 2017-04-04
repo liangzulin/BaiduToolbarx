6862c8415d9a0bd35b2c921ba1f39a7a055b4a8c
liangzulin <lllzzzlll@hotmail.com>
2017-04-04 21:49:51 +0900

    first netbeas toolbar
    first netbeas toolbar

---
+++ build/classes-generated/org/myorg/baidutoolbar/Bundle.java
@@ -0,0 +1,13 @@
+package org.myorg.baidutoolbar;
+/** Localizable strings for {@link org.myorg.baidutoolbar}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Baidu</i>
+     * @see BaiduActionListener
+     */
+    static String CTL_BaiduActionListener() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_BaiduActionListener");
+    }
+    private void Bundle() {}
+}
---
+++ src/org/myorg/baidutoolbar/BaiduActionListener.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.myorg.baidutoolbar;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "File",
+        id = "org.myorg.baidutoolbar.BaiduActionListener"
+)
+@ActionRegistration(
+        iconBase = "org/myorg/baidutoolbar/baidu2.gif",
+        displayName = "#CTL_BaiduActionListener"
+)
+@ActionReference(path = "Toolbars/File", position = 0)
+@Messages("CTL_BaiduActionListener=Baidu")
+public final class BaiduActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
